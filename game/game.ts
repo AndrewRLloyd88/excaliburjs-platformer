59c9b8210e2c4ac82ddbea33d72fdee9a4dd5eaf
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-14 12:58:55 -0800

    init commit

---
+++ game/game.ts
@@ -0,0 +1,24 @@
+/// <reference path="../node_modules/excalibur/dist/excalibur.d.ts" />
+
+let game = new ex.Engine({
+  backgroundColor: ex.Color.fromHex('#ff5e40'),
+  width: 800,
+  height: 600,
+});
+
+// create an asset loader
+let loader = new ex.Loader();
+let resources = {
+  /* include resources here */
+  //txPlayer: new ex.Texture("assets/tex/player.png")
+};
+
+// queue resources for loading
+for (let r in resources) {
+  loader.addResource(resources[r]);
+}
+
+// uncomment loader after adding resources
+game.start(loader).then(() => {
+  // start your game!
+});
